76dc278ead29d551e62e5026f37513694df3476e
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-05 18:08:22 -0400

    adds body parser and static server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 const PORT = process.env.PORT || 3000
 
